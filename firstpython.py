06597d90c746e1dc015ab88a9b59467ff5d8588e
M Sufyan Mughal <mskhalid703@gmail.com>
2025-01-06 12:04:24 +0500

    1 hour

--- firstpython.py
+++ firstpython.py
@@ -5,3 +5,18 @@ age=18
 print(hello,
       "my name is :", type(name),
       "and age is :",age,)
+#integer multiplication 
+# answer will roundoff actual than lesser 
+a=1.5
+b=3
+c=a//b
+print(c,a/b)
+#  if we multiply the string to number then answer will be string multiply with the number 
+a="Aa"
+b=2
+print (a*2)
+# if anything is in bracket then it solve first then it multiply and divide with other 
+a=22
+b=33
+c=44
+print ((a+b)*c)
