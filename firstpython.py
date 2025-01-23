dbb3d9bf3d6bfea1b6d506ba53f1ced61e7ae1c4
M Sufyan Mughal <mskhalid703@gmail.com>
2025-01-23 17:46:14 +0500

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -1,33 +1,40 @@
-# day one of learning python 
-hello ='hi'
-name = 'sufyan mughal' 
-age=18
-print(hello,
-      "my name is :", type(name),
-      "and age is :",age,)
-#integer multiplication 
-# answer will roundoff actual than lesser 
-a=1.5
-b=3
-c=a//b
-print(c,a/b)
-#  if we multiply the string to number then answer will be string multiply with the number 
-a="Aa"
-b=2
-print (a*2)
-# if anything is in bracket it solve first then it multiply and divide with other 
-a=22
-b=33
-c=44
-print ((a+b)*c)
-# how to take input in string 
-name = input("helolo")
-print(name)
-# this is how you take input in number 
-age = int(input("age:"))
-print (age)
-price = str and float(input("set :"))
-print(price)
+# # day one of learning python 
+# hello ='hi'
+# name = 'sufyan mughal' 
+# age=18
+# print(hello,
+#       "my name is :", type(name),
+#       "and age is :",age,)
+# #integer multiplication 
+# # answer will roundoff actual than lesser 
+# a=1.5
+# b=3
+# c=a//b
+# print(c,a/b)
+# #  if we multiply the string to number then answer will be string multiply with the number 
+# a="Aa"
+# b=2
+# print (a*2)
+# # if anything is in bracket it solve first then it multiply and divide with other 
+# a=22
+# b=33
+# c=44
+# print ((a+b)*c)
+# # how to take input in string 
+# name = input("helolo")
+# print(name)
+# # this is how you take input in number 
+# age = int(input("age:"))
+# print (age)
+# price = str and float(input("set :"))
+# print(price)
 # if else 
-# if ():
-# else:
+light = input("light:")
+if (light == "red "):
+      print ("stop")
+elif(light == "green"):
+      print ("go")
+elif (light == "yellow "):
+      print ("ready")
+else: 
+      print ("light is not working")
