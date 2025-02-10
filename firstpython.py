59a8b2b55030f59355541552d3f88782190ad707
M Sufyan Mughal <mskhalid703@gmail.com>
2025-02-10 11:19:20 +0500

    python aj se phir shoro

--- firstpython.py
+++ firstpython.py
@@ -1,40 +1,99 @@
-# # day one of learning python 
-# hello ='hi'
-# name = 'sufyan mughal' 
-# age=18
-# print(hello,
-#       "my name is :", type(name),
-#       "and age is :",age,)
-# #integer multiplication 
-# # answer will roundoff actual than lesser 
-# a=1.5
-# b=3
-# c=a//b
-# print(c,a/b)
-# #  if we multiply the string to number then answer will be string multiply with the number 
-# a="Aa"
-# b=2
-# print (a*2)
-# # if anything is in bracket it solve first then it multiply and divide with other 
+# # # day one of learning python 
+# # hello ='hi'
+# # name = 'sufyan mughal' 
+# # age=18
+# # print(hello,
+# #       "my name is :", type(name),
+# #       "and age is :",age,)
+# # #integer multiplication 
+# # # answer will roundoff actual than lesser 
+# # a=1.5
+# # b=3
+# # c=a//b
+# # print(c,a/b)
+# # #  if we multiply the string to number then answer will be string multiply with the number 
+# # a="Aa"
+# # b=2
+# # print (a*2)
+# # # if anything is in bracket it solve first then it multiply and divide with other 
+# # a=22
+# # b=33
+# # c=44
+# # print ((a+b)*c)
+# # # how to take input in string 
+# # name = input("helolo")
+# # print(name)
+# # # this is how you take input in number 
+# # age = int(input("age:"))
+# # print (age)
+# # price = str and float(input("set :"))
+# # print(price)
+# # if else 
+# # light = input("light:")
+# # if (light == "red "):
+# #       print ("stop")
+# # elif(light == "green"):
+# #       print ("go")
+# # elif (light == "yellow "):
+# #       print ("ready")
+# # else: 
+# #       print ("light is not working")
+# # ternary operator 
+# # eat = "he"
+# # print ("khalo") if (eat =="biryani") else print("nhi kaho")
+# # clever if 
+# # age =int(input("age:"))
+# # vote = ("no","yes") [age >= 18]
+# # print(vote)
+# # arithmatic operator 
 # a=22
-# b=33
-# c=44
-# print ((a+b)*c)
-# # how to take input in string 
-# name = input("helolo")
-# print(name)
-# # this is how you take input in number 
-# age = int(input("age:"))
-# print (age)
-# price = str and float(input("set :"))
-# print(price)
-# if else 
-light = input("light:")
-if (light == "red "):
-      print ("stop")
-elif(light == "green"):
-      print ("go")
-elif (light == "yellow "):
-      print ("ready")
-else: 
-      print ("light is not working")
+# b=5
+# # print(a+b)
+# # print(a-b)
+# # print(a*b)
+# # print(a**b)
+# # print(a/b)
+# # print(a%b)
+# # relational operator
+# # print(a == b)
+# # print (a != b) 
+# # print (a > b)
+# # print (a < b)
+# # print (a >= b)
+# # print (a <= b)
+
+# # assignment operator 
+# num =10 
+# num += 10
+# print(num)
+# num =10 
+# num -= 10
+# print(num)
+# num =10 
+# num *= 10
+# print(num)
+# num =10 
+# num **= 10
+# print(num)
+# num =10 
+# num /= 10
+# print(num)
+# num =10 
+# num %= 10
+# print(num)
+# logical operator
+A =  20
+B =29
+print (not False )
+print (not True )
+print (not (A > B))
+
+va1 = True 
+va2 = False
+print ("and operator :",va1 and va2)
+print ("or operator ",va1 or va2) 
+# type casting 
+a =int("1")
+b = 2
+print(a+b)
+
